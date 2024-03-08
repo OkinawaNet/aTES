67c8e1237cba5df75b7755844b0770e56b21b7e4
Яков Сергеев <y.sergeev@fun-box.ru>
2024-03-08 04:27:20 +0400

    [HW2] docker-compose

---
+++ db/init.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE ates_keycloak;
+GRANT ALL PRIVILEGES ON DATABASE ates_keycloak TO postgres;
+
+CREATE DATABASE ates_task_tracker;
+GRANT ALL PRIVILEGES ON DATABASE ates_task_tracker TO postgres;
