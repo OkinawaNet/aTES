f5659b1aa9026992eef4dd7fc5d0e5adfea58e77
Яков Сергеев <y.sergeev@fun-box.ru>
2024-03-12 02:46:12 +0400

    [HW2] task_tracker init commit

--- db/init.sql
+++ db/init.sql
@@ -1,5 +1,8 @@
 CREATE DATABASE ates_keycloak;
 GRANT ALL PRIVILEGES ON DATABASE ates_keycloak TO postgres;
 
-CREATE DATABASE ates_task_tracker;
-GRANT ALL PRIVILEGES ON DATABASE ates_task_tracker TO postgres;
+CREATE DATABASE ates_task_tracker_development;
+GRANT ALL PRIVILEGES ON DATABASE ates_task_tracker_development TO postgres;
+
+CREATE DATABASE ates_task_tracker_test;
+GRANT ALL PRIVILEGES ON DATABASE ates_task_tracker_test TO postgres;
