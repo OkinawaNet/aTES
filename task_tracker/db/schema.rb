ef52d68af20e7e12ebe4cae92848800d1e93c85a
Яков Сергеев <y.sergeev@fun-box.ru>
2024-03-12 06:18:42 +0400

    [HW2] fix

--- task_tracker/db/schema.rb
+++ task_tracker/db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_12_020120) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_12_021423) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
+    t.string "email", default: ""
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_12_020120) do
     t.string "last_name"
     t.string "provider"
     t.string "uid"
+    t.string "password"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
