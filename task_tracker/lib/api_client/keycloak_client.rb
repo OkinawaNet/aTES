8415a80c703832caef340952045e11409bb316af
Яков Сергеев <y.sergeev@fun-box.ru>
2024-03-13 04:57:02 +0400

    [HW2] keycloak api client

--- task_tracker/lib/api_client/keycloak_client.rb
+++ task_tracker/lib/api_client/keycloak_client.rb
@@ -14,6 +14,48 @@ module ApiClient
       @client_id = client_id
     end
 
+    def get_webhooks
+      get_webhooks.select { |webhook| webhook['url'] == Settings.root_url }
+    end
+
+    def create_webhook(event_types = ['*'], enabled = true)
+      post('/webhooks', {
+        "enabled": enabled,
+        "url": Settings.root_url,
+        "eventTypes": event_types
+      })
+    end
+
+    def delete_webhook(id)
+      delete("webhooks/#{id}")
+    end
+
+    private
+
+    def get_all_webhooks
+      JSON.parse(get('/webhooks').body)
+    end
+
+    def get(path)
+      token = get_admin_token
+      send_request('get', path, nil, token)
+    end
+
+    def put(path, body)
+      token = get_admin_token
+      send_request('put', path, body, token)
+    end
+
+    def post(path, body)
+      token = get_admin_token
+      send_request('post', path, body, token)
+    end
+
+    def delete(path)
+      token = get_admin_token
+      send_request('delete', path, nil, token)
+    end
+
     def get_admin_token
       response = send_request(
         :post,
@@ -24,6 +66,7 @@ module ApiClient
           username: @admin_credentials.username,
           password: @admin_credentials.password
         },
+        nil,
         'application/x-www-form-urlencoded'
       )
 
@@ -32,9 +75,7 @@ module ApiClient
       "Bearer #{result['access_token']}"
     end
 
-    private
-
-    def send_request(action, path, body, type = 'application/json', token = nil)
+    def send_request(action, path, body, token = nil, type = 'application/json')
       parameters = prepare_body(type, body)
 
       response = connection.public_send(action) do |req|
