01bc523e6895f61122c4c70723d86d82b24c67e1
Яков Сергеев <y.sergeev@fun-box.ru>
2024-03-16 00:37:28 +0400

    [HW2] finish rename accounting

--- accounting/config/application.rb
+++ accounting/config/application.rb
@@ -7,7 +7,7 @@ require 'httplog'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module TaskTracker
+module Accounting
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
