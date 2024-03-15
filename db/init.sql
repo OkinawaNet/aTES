a70dc559ddab46049ada479b78f2424bac0c6be8
Яков Сергеев <y.sergeev@fun-box.ru>
2024-03-15 23:55:04 +0400

    [HW2] docker-compose accounting settings + db init

--- db/init.sql
+++ db/init.sql
@@ -6,3 +6,9 @@ GRANT ALL PRIVILEGES ON DATABASE ates_task_tracker_development TO postgres;
 
 CREATE DATABASE ates_task_tracker_test;
 GRANT ALL PRIVILEGES ON DATABASE ates_task_tracker_test TO postgres;
+
+CREATE DATABASE ates_accounting_development;
+GRANT ALL PRIVILEGES ON DATABASE ates_accounting_development TO postgres;
+
+CREATE DATABASE ates_accounting_test;
+GRANT ALL PRIVILEGES ON DATABASE ates_accounting_test TO postgres;
