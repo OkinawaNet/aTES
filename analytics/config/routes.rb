bc51fd6b4df62dc2bf6c703ecad5fa7dbe6c4b81
Яков Сергеев <y.sergeev@fun-box.ru>
2024-03-23 20:12:03 +0400

    [HW2] stats controllers + templates

--- analytics/config/routes.rb
+++ analytics/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  get 'stats/index'
+
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -11,5 +12,5 @@ Rails.application.routes.draw do
   post 'callbacks/keycloak_events', to: 'callbacks#keycloak_events'
 
   # Defines the root path route ("/")
-  root :to => "home#index"
+  root :to => "stats#index"
 end
